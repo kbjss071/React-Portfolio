4f5b0f0853ebf4dac1b929ef1ffc997a097b4cec
Jin Kang <kbjss071@gmail.com>
2023-03-02 17:56:43 -0500

    added copyright and rearranged footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,35 @@
 import React from "react";
 import {FaGithub, FaLinkedin, FaStackOverflow} from "react-icons/fa"
+import {Container, Row, Col} from 'react-bootstrap'
 import "../styles/Footer.css"
 import { Icon } from '@iconify/react';
 
 function Footer(){
-    const style = {fontSize: "4rem"}
+    const style = {fontSize: "1.5rem"}
+    let date = new Date()
+    let year = date.getFullYear();
 
     return (
-        <div className="container">
-            <h1>More about me</h1>
-            <ul className="list-style">
-                <li><a href="https://github.com/kbjss071" target="_blank" rel="noreferrer"><FaGithub style={style}/></a></li>
-                <li><a href="https://www.linkedin.com/in/jin-kang-072b3a127/" target="_blank" rel="noreferrer"><FaLinkedin style={style}/></a></li>
-                <li><a href="https://stackoverflow.com/users/19080416/jin-kang" target="_blank" rel="noreferrer"><FaStackOverflow style={style}/></a></li>
-                <li><a href="https://leetcode.com/kbjkbc123/" target="_blank" rel="noreferrer"><Icon icon="simple-icons:leetcode" style={style}/></a></li>
-            </ul>
-        </div>        
+        <Container fluid className="footer">
+            <Row>
+                <Col md="4" className="footer-center">
+                    <h3>Designed and Developed by Jin Kang</h3>
+                </Col>
+                <Col md="4" className="footer-center">
+                    <h3>Copyright © {year}</h3>
+                </Col>
+                <Col md="4" className="footer-center footer-icon">
+                    {/* <h1>More about me</h1> */}
+                    <ul className="list-style">
+                        <li><a href="https://github.com/kbjss071" target="_blank" rel="noreferrer"><FaGithub style={style}/></a></li>
+                        <li><a href="https://www.linkedin.com/in/jin-kang-072b3a127/" target="_blank" rel="noreferrer"><FaLinkedin style={style}/></a></li>
+                        <li><a href="https://stackoverflow.com/users/19080416/jin-kang" target="_blank" rel="noreferrer"><FaStackOverflow style={style}/></a></li>
+                        <li><a href="https://leetcode.com/kbjkbc123/" target="_blank" rel="noreferrer"><Icon icon="simple-icons:leetcode" style={style}/></a></li>
+                    </ul>
+
+                </Col>
+            </Row>
+        </Container>        
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Navbar.css';
-import Container from 'react-bootstrap/Container';
+import {Container, Row, Col} from 'react-bootstrap/';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
@@ -10,17 +10,23 @@ function NavBar(props) {
   const brandStyle = {fontSize: "3rem", color:"white", display:"inline-block"}
   return (
     <Navbar>
-      <Container>
-        <Navbar.Brand style={brandStyle}><Link to="/">JK</Link></Navbar.Brand>
-        <Navbar.Collapse style={navbarStyle}>
-          <Nav>
-              <Nav.Link className='mx-2' as={Link} to="React-Portfolio">Home</Nav.Link>
-              <Nav.Link className='mx-2' as={Link} to="About">About</Nav.Link>
-              <Nav.Link className='mx-2' as={Link} to="Portfolio">Portfolio</Nav.Link>
-              <Nav.Link className='mx-2' as={Link} to="Contact">Contact</Nav.Link>
-              <Nav.Link className='mx-2' as={Link} to="Resume">Resume</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Container fluid className='container'>
+        <Row>
+          <Col md="4">
+            <Navbar.Brand style={brandStyle}><Link to="/">JK</Link></Navbar.Brand>
+          </Col>
+          <Col md="8" className='right'>
+            <Navbar.Collapse>
+              <Nav>
+                  <Nav.Link className='mx-2' as={Link} to="React-Portfolio">Home</Nav.Link>
+                  <Nav.Link className='mx-2' as={Link} to="About">About</Nav.Link>
+                  <Nav.Link className='mx-2' as={Link} to="Portfolio">Portfolio</Nav.Link>
+                  <Nav.Link className='mx-2' as={Link} to="Contact">Contact</Nav.Link>
+                  <Nav.Link className='mx-2' as={Link} to="Resume">Resume</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
