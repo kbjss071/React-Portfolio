44d2cd4f70c2ca71cb7fd285bd45d964e46f2ccf
Jin Kang <kbjss071@gmail.com>
2023-03-29 15:52:35 -0400

    navbar expanded feature'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,52 +6,25 @@ import {AiOutlineHome, AiOutlineUser, AiOutlineDesktop, AiOutlineComment, AiOutl
 import {FaBars} from 'react-icons/fa'
 
 function NavBar(props) {
-  // const brandStyle = {fontSize: "3rem", color:"white", display:"inline-block"}
-
-  const [isOpen, setIsOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleToggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setExpanded(!expanded);
+  };
 
 
   return (
-    // <Navbar collapseOnSelect className='navbar-expand-lg fixed-top navbar-light'>
-    //   <Container fluid>
-    //         <Navbar.Brand><Link to="/React-Portfolio">JK</Link></Navbar.Brand>
-    //         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-    //         <Navbar.Collapse className='collapse' id='responsive-navbar-nav'>
-    //           <Nav className='ms-auto right'>
-    //             <Nav.Item>
-    //               <Nav.Link className='mx-2' as={Link} to="React-Portfolio"><AiOutlineHome /><span>Home</span></Nav.Link>
-    //             </Nav.Item>
-    //             <Nav.Item>
-    //               <Nav.Link className='mx-2' as={Link} to="About"><AiOutlineUser /><span>About</span></Nav.Link> 
-    //             </Nav.Item>
-    //             <Nav.Item>
-    //               <Nav.Link className='mx-2' as={Link} to="Portfolio"><AiOutlineDesktop /><span>Portfolio</span></Nav.Link>
-    //             </Nav.Item>
-    //             <Nav.Item>
-    //               <Nav.Link className='mx-2' as={Link} to="Contact"><AiOutlineComment /><span>Contact</span></Nav.Link>                  
-    //             </Nav.Item>
-    //             <Nav.Item>
-    //               <Nav.Link className='mx-2' as={Link} to="Resume"><AiOutlineFileText /><span>Resume</span></Nav.Link>                  
-    //             </Nav.Item>
-    //           </Nav>
-    //         </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect fixed='top' expand="lg" bg="dark" variant="dark" expanded={expanded} onToggle={handleToggle}>
       <Container>
         <Navbar.Brand href="/React-Portfolio">JK</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className='ms-auto right'>
-            <Nav.Link className='mx-2' as={Link} to='React-Portfolio'><AiOutlineHome /><span>Home</span></Nav.Link>
-            <Nav.Link className='mx-2' as={Link} to="About"><AiOutlineUser /><span>About</span></Nav.Link>
-            <Nav.Link className='mx-2' as={Link} to='Portfolio'><AiOutlineDesktop /><span>Portfolio</span></Nav.Link>
-            <Nav.Link className='mx-2' as={Link} to='Contact'><AiOutlineComment /><span>Contact</span></Nav.Link>
-            <Nav.Link className='mx-2' as={Link} to='Resume'><AiOutlineFileText /><span>Resume</span></Nav.Link>
+          <Nav className='mr-auto right'>
+            <Nav.Link className='mx-2' as={Link} to='React-Portfolio' onClick={handleToggle}><AiOutlineHome /><span>Home</span></Nav.Link>
+            <Nav.Link className='mx-2' as={Link} to="About" onClick={handleToggle}><AiOutlineUser /><span>About</span></Nav.Link>
+            <Nav.Link className='mx-2' as={Link} to='Portfolio' onClick={handleToggle}><AiOutlineDesktop /><span>Portfolio</span></Nav.Link>
+            <Nav.Link className='mx-2' as={Link} to='Contact' onClick={handleToggle}><AiOutlineComment /><span>Contact</span></Nav.Link>
+            <Nav.Link className='mx-2' as={Link} to='Resume' onClick={handleToggle}><AiOutlineFileText /><span>Resume</span></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
